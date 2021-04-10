77a77f1f09999ce7f4fb7775517a4d66faf3e8a3
Luca Martinelli <lucxmangajet@gmail.com>
2021-04-10 10:49:42 +0200

    setup dialog runner to run from converter

--- Assets/Scripts/DialogRunner.cs
+++ Assets/Scripts/DialogRunner.cs
@@ -7,24 +7,65 @@ using UnityEngine.Serialization;
 public class DialogRunner : MonoBehaviour
 {
     [SerializeField] DialogDisplayer dialogDisplayer;
+    [SerializeField] SceneLoader sceneLoader;
 
     [SerializeField] DialogSection[] manualTestDialog;
-    [SerializeField] bool runManualDialog;
+    [SerializeField] TextAsset dialogSceneScript;
 
+    [SerializeField] RunSelection runSelection;
 
     private void Start()
     {
-        if (runManualDialog)
-            StartCoroutine(RunDialogFromSecionArray(manualTestDialog));
+        RunDialog();
+    }
+
+    private void RunDialog()
+    {
+        switch (runSelection)
+        {
+            case RunSelection.Manual:
+                StartCoroutine(RunDialogFromSecionArray(manualTestDialog));
+                break;
+
+            case RunSelection.Random:
+                if (sceneLoader != null)
+                {
+                    DialogSection[] sections = DialogSceneScriptConverter.Convert(sceneLoader.GetRandomScene());
+                    StartCoroutine(RunDialogFromSecionArray(sections));
+                }
+                else
+                {
+                    Debug.LogError("DialogRunner: Trying to run random dialog with no scene loader assigned");
+                }
+                break;
+
+            case RunSelection.FromFile:
+                if (dialogSceneScript != null)
+                {
+                    DialogSection[] sections = DialogSceneScriptConverter.Convert(dialogSceneScript.text);
+                    StartCoroutine(RunDialogFromSecionArray(sections));
+                }
+                else
+                {
+                    Debug.LogError("DialogRunner: Trying to run dialog with no dialog script assigned");
+                }
+                break;
+        }
     }
 
     private IEnumerator RunDialogFromSecionArray(DialogSection[] dialogSections)
     {
         Debug.Log("Running Test");
 
-        if(dialogDisplayer == null)
+        if (dialogDisplayer == null)
         {
             Debug.LogError("No DialogDisplayer assigned.");
+            yield break;
+        }
+
+        if (dialogSections == null)
+        {
+            Debug.LogError("Trying to run from empty dialog.");
             yield break;
         }
 
@@ -39,6 +80,13 @@ public class DialogRunner : MonoBehaviour
         }
 
         Debug.Log("Running Test finished.");
+    }
+
+    public enum RunSelection
+    {
+        Manual,
+        Random,
+        FromFile
     }
 
 }
---
+++ Assets/Scripts/DialogSceneScriptConverter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DialogSceneScriptConverter 
+{
+    
+    public static DialogSection[] Convert(string s)
+    {
+
+        return null;
+    }
+
+}
